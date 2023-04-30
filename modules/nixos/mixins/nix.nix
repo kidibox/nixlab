ebf198cafa3f0b2678871eb876b570de95dacbb9
Arnaud Rebts <arnaud.rebts@gmail.com>
2023-04-30 11:38:21 +0200

    ci: enable cachix

--- modules/nixos/mixins/nix.nix
+++ modules/nixos/mixins/nix.nix
@@ -3,6 +3,16 @@
     settings = {
       allowed-users = [ "root" "@wheel" ];
       trusted-users = [ "root" "@wheel" ];
+      trusted-substituters = [
+        "https://nix-community.cachix.org"
+        "https://numtide.cachix.org"
+        "https://kidibox.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+        "kidibox.cachix.org-1:BN875x9JUW61souPxjf7eA5Uh2k3A1OSA1JIb/axGGE="
+      ];
     };
     extraOptions = ''
       experimental-features = nix-command flakes ca-derivations
