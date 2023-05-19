5866ed108a7ff8901824345e91accf1e856abb11
Arnaud Rebts <arnaud.rebts@gmail.com>
2023-05-19 17:54:39 +0200

    fix: move networking setup into a separate file

--- hosts/hypernix/default.nix
+++ hosts/hypernix/default.nix
@@ -6,6 +6,7 @@
     inputs.impermanence.nixosModule
     ../../modules/common.nix
     ./hardware-configuration.nix
+    ./networking.nix
   ];
 
   nixpkgs.hostPlatform.system = "x86_64-linux";
@@ -24,79 +25,5 @@
     # required by ZFS
     hostId = "385f9236";
     hostName = "hypernix";
-    useDHCP = lib.mkForce false;
-  };
-
-  systemd.network = {
-    netdevs = {
-      br10 = {
-        netdevConfig = {
-          Name = "br10";
-          Kind = "bridge";
-        };
-      };
-      br100 = {
-        netdevConfig = {
-          Name = "br100";
-          Kind = "bridge";
-        };
-      };
-      srv = {
-        netdevConfig = {
-          Name = "srv";
-          Kind = "vlan";
-        };
-        vlanConfig.Id = 10;
-      };
-      lan = {
-        netdevConfig = {
-          Name = "lan";
-          Kind = "vlan";
-        };
-        vlanConfig.Id = 100;
-      };
-    };
-    networks = {
-      br10 = {
-        name = "br10";
-        DHCP = "ipv4";
-      };
-      br100 = {
-        name = "br100";
-        linkConfig = {
-          RequiredForOnline = false;
-        };
-        networkConfig = {
-          LinkLocalAddressing = "no";
-          LLDP = "no";
-          EmitLLDP = "no";
-          IPv6AcceptRA = "no";
-          IPv6SendRA = "no";
-        };
-      };
-      enp3s0 = {
-        name = "enp3s0";
-        vlan = [ "srv" "lan" ];
-        linkConfig = {
-          RequiredForOnline = false;
-        };
-        # Disable all the autoconfiguration magic we don't need a link without VLAN
-        networkConfig = {
-          LinkLocalAddressing = "no";
-          LLDP = "no";
-          EmitLLDP = "no";
-          IPv6AcceptRA = "no";
-          IPv6SendRA = "no";
-        };
-      };
-      srv = {
-        name = "srv";
-        bridge = [ "br10" ];
-      };
-      lan = {
-        name = "lan";
-        bridge = [ "br100" ];
-      };
-    };
   };
 }
---
+++ hosts/hypernix/networking.nix
@@ -0,0 +1,76 @@
+{
+  networking.useDHCP = lib.mkForce false;
+  systemd.network = {
+    netdevs = {
+      br10 = {
+        netdevConfig = {
+          Name = "br10";
+          Kind = "bridge";
+        };
+      };
+      br100 = {
+        netdevConfig = {
+          Name = "br100";
+          Kind = "bridge";
+        };
+      };
+      srv = {
+        netdevConfig = {
+          Name = "srv";
+          Kind = "vlan";
+        };
+        vlanConfig.Id = 10;
+      };
+      lan = {
+        netdevConfig = {
+          Name = "lan";
+          Kind = "vlan";
+        };
+        vlanConfig.Id = 100;
+      };
+    };
+    networks = {
+      br10 = {
+        name = "br10";
+        DHCP = "ipv4";
+      };
+      br100 = {
+        name = "br100";
+        linkConfig = {
+          RequiredForOnline = false;
+        };
+        networkConfig = {
+          LinkLocalAddressing = "no";
+          LLDP = "no";
+          EmitLLDP = "no";
+          IPv6AcceptRA = "no";
+          IPv6SendRA = "no";
+        };
+      };
+      enp3s0 = {
+        name = "enp3s0";
+        vlan = [ "srv" "lan" ];
+        linkConfig = {
+          RequiredForOnline = false;
+        };
+        # Disable all the autoconfiguration magic we don't need a link without VLAN
+        networkConfig = {
+          LinkLocalAddressing = "no";
+          LLDP = "no";
+          EmitLLDP = "no";
+          IPv6AcceptRA = "no";
+          IPv6SendRA = "no";
+        };
+      };
+      srv = {
+        name = "srv";
+        bridge = [ "br10" ];
+      };
+      lan = {
+        name = "lan";
+        bridge = [ "br100" ];
+      };
+    };
+  };
+
+}
