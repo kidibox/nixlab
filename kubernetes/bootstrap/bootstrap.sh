fdf702886e11ee64492437dc8567be3ee899470f
Arnaud Rebts <arnaud.rebts@gmail.com>
2023-11-02 16:36:25 +0100

    fix

--- kubernetes/bootstrap/bootstrap.sh
+++ kubernetes/bootstrap/bootstrap.sh
@@ -4,7 +4,7 @@ repo_root=$(git rev-parse --show-toplevel)
 
 (
 	cd "$repo_root" || exit 1
-	kubectl apply --server-side --kustomize ./kubernetes/bootstrap/flux --force-conflicts
+	kubectl apply --server-side --kustomize ./kubernetes/bootstrap/flux
 	sops --decrypt ./kubernetes/bootstrap/flux/age-key.sops.yaml | kubectl apply -f -
 	sops --decrypt ./kubernetes/bootstrap/flux/github-deploy-key.sops.yaml | kubectl apply -f -
 	kubectl apply --server-side --kustomize ./kubernetes/flux/config
