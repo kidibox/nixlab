e986adffedc00da8b03e35fb6d1e352ebb4804b0
Arnaud Rebts <arnaud.rebts@gmail.com>
2023-11-02 15:34:37 +0100

    test fluxcd

---
+++ kubernetes/bootstrap/bootstrap.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+repo_root=$(git rev-parse --show-toplevel)
+
+(
+	cd "$repo_root" || exit 1
+	kubectl apply --server-side --kustomize ./kubernetes/bootstrap/flux --force-conflicts
+	sops --decrypt ./kubernetes/bootstrap/flux/age-key.sops.yaml | kubectl apply -f -
+	sops --decrypt ./kubernetes/bootstrap/flux/github-deploy-key.sops.yaml | kubectl apply -f -
+	kubectl apply --server-side --kustomize ./kubernetes/flux/config
+)
