9c6e9355c6c5b0e9a1fac89b140d9cb06ab832ec
Arnaud Rebts <arnaud.rebts@gmail.com>
2023-11-07 22:24:23 +0100

    fix: simplify flake & host management

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,5 +1,38 @@
-{
-  imports = [
-    ./hypernix
+{ self, inputs, lib, ... }:
+let
+  specialArgs = { inherit inputs self; };
+
+  items = builtins.removeAttrs (builtins.readDir (./.)) [
+    "default.nix"
+    # We don't need a nixos config for it
+    # "installer.nix"
   ];
+
+  build = (path:
+    let
+      hostName = builtins.replaceStrings [ ".nix" ] [ "" ] path;
+    in
+    {
+      name = hostName;
+      value = inputs.nixpkgs.lib.nixosSystem
+        {
+          inherit specialArgs;
+
+          modules = [
+            inputs.disko.nixosModules.disko
+            {
+              imports = [ ./${path} ];
+
+              nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+              networking = {
+                inherit hostName;
+              };
+            }
+          ];
+        };
+    });
+in
+{
+  flake.nixosConfigurations = builtins.listToAttrs (builtins.map build (builtins.attrNames items));
 }
