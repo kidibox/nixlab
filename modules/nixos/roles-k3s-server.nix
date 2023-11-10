1bc405502e11d1eb026f1d3dbabaa6e7d83678f5
Arnaud Rebts <arnaud.rebts@gmail.com>
2023-11-10 23:04:00 +0100

    comment unused code

--- modules/nixos/roles-k3s-server.nix
+++ modules/nixos/roles-k3s-server.nix
@@ -14,95 +14,95 @@ let
     '';
   };
 
-  ciliumHelmChart = pkgs.writeTextFile {
-    name = "cilium.yaml";
-    text = ''
-      apiVersion: helm.cattle.io/v1
-      kind: HelmChart
-      metadata:
-        name: cilium
-        namespace: kube-system
-      spec:
-        bootstrap: true
-        repo: https://helm.cilium.io/
-        chart: cilium
-        version: 1.14.3
-        targetNamespace: kube-system
-        valuesContent: |-
-          k8sServiceHost: 10.0.10.20
-          k8sServicePort: 6443
-          kubeProxyReplacement: true
+  # ciliumHelmChart = pkgs.writeTextFile {
+  #   name = "cilium.yaml";
+  #   text = ''
+  #     apiVersion: helm.cattle.io/v1
+  #     kind: HelmChart
+  #     metadata:
+  #       name: cilium
+  #       namespace: kube-system
+  #     spec:
+  #       bootstrap: true
+  #       repo: https://helm.cilium.io/
+  #       chart: cilium
+  #       version: 1.14.3
+  #       targetNamespace: kube-system
+  #       valuesContent: |-
+  #         k8sServiceHost: 10.0.10.20
+  #         k8sServicePort: 6443
+  #         kubeProxyReplacement: true
+  #
+  #         # k3s specific
+  #         cni:
+  #           binPath: /var/lib/rancher/k3s/agent/opt/cni/bin
+  #           confPath: /var/lib/rancher/k3s/agent/etc/cni/net.d
+  #
+  #         bpf:
+  #           # does this makes senses with native routing?
+  #           masquerade: true
+  #
+  #         # this breaks ingress on same node
+  #         # routingMode: native
+  #         # ipv4NativeRoutingCIDR: 10.42.0.0/16
+  #         # auto-direct-node-routes: true
+  #         # endpointRoutes:
+  #         #   enabled: true
+  #
+  #         bgpControlPlane:
+  #           enabled: true
+  #         operator:
+  #           replicas: 1
+  #         ipam:
+  #           operator:
+  #             clusterPoolIPv4PodCIDRList:
+  #               - 10.42.0.0/16
+  #
+  #
+  #         ingressController:
+  #           enabled: true
+  #           loadbalancerMode: dedicated
+  #         gatewayAPI:
+  #           enabled: true
+  #
+  #         hubble:
+  #           ui:
+  #             enabled: true
+  #           relay:
+  #             enabled: true
+  #   '';
+  # };
 
-          # k3s specific
-          cni:
-            binPath: /var/lib/rancher/k3s/agent/opt/cni/bin
-            confPath: /var/lib/rancher/k3s/agent/etc/cni/net.d
-
-          bpf:
-            # does this makes senses with native routing?
-            masquerade: true
-
-          # this breaks ingress on same node
-          # routingMode: native
-          # ipv4NativeRoutingCIDR: 10.42.0.0/16
-          # auto-direct-node-routes: true
-          # endpointRoutes:
-          #   enabled: true
-
-          bgpControlPlane:
-            enabled: true
-          operator:
-            replicas: 1
-          ipam:
-            operator:
-              clusterPoolIPv4PodCIDRList:
-                - 10.42.0.0/16
-
-
-          ingressController:
-            enabled: true
-            loadbalancerMode: dedicated
-          gatewayAPI:
-            enabled: true
-
-          hubble:
-            ui:
-              enabled: true
-            relay:
-              enabled: true
-    '';
-  };
-
-  ciliumExtra = pkgs.writeTextFile
-    {
-      name = "cilium-extra.yaml";
-      text = ''
-        apiVersion: "cilium.io/v2alpha1"
-        kind: CiliumLoadBalancerIPPool
-        metadata:
-          name: "default"
-        spec:
-          cidrs:
-            - cidr: "10.0.50.0/24"
-        ---
-        apiVersion: cilium.io/v2alpha1
-        kind: CiliumBGPPeeringPolicy
-        metadata:
-          name: bgp-peering-policy
-        spec:
-          nodeSelector:
-            matchLabels: {}
-          virtualRouters:
-            - localASN: 64512
-              exportPodCIDR: true
-              neighbors:
-                - peerAddress: '10.0.10.1/32'
-                  peerASN: 64512
-              serviceSelector:
-                matchExpressions:
-                  - {key: somekey, operator: NotIn, values: ['never-used-value']}
-      '';
-    };
+  # ciliumExtra = pkgs.writeTextFile
+  #   {
+  #     name = "cilium-extra.yaml";
+  #     text = ''
+  #       apiVersion: "cilium.io/v2alpha1"
+  #       kind: CiliumLoadBalancerIPPool
+  #       metadata:
+  #         name: "default"
+  #       spec:
+  #         cidrs:
+  #           - cidr: "10.0.50.0/24"
+  #       ---
+  #       apiVersion: cilium.io/v2alpha1
+  #       kind: CiliumBGPPeeringPolicy
+  #       metadata:
+  #         name: bgp-peering-policy
+  #       spec:
+  #         nodeSelector:
+  #           matchLabels: {}
+  #         virtualRouters:
+  #           - localASN: 64512
+  #             exportPodCIDR: true
+  #             neighbors:
+  #               - peerAddress: '10.0.10.1/32'
+  #                 peerASN: 64512
+  #             serviceSelector:
+  #               matchExpressions:
+  #                 - {key: somekey, operator: NotIn, values: ['never-used-value']}
+  #     '';
+  #   };
 in
 {
   options.nixlab.k3s = {
